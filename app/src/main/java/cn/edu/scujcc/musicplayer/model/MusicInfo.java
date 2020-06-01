bb150a67923206613acd1b413b3567f9171d0e48
Zhang <44764799+outmans@users.noreply.github.com>
2020-06-01 15:07:54 +0800

    查询本地音乐且播放的功能已完成

---
+++ app/src/main/java/cn/edu/scujcc/musicplayer/model/MusicInfo.java
@@ -0,0 +1,51 @@
+package cn.edu.scujcc.musicplayer.model;
+
+import java.io.Serializable;
+
+public class MusicInfo implements Serializable {
+    private String musicTitle;
+    private String musicName;
+    private String musicPath;
+    private String musicArtist;
+    private int musicDuration;
+
+    public String getMusicTitle() {
+        return musicTitle;
+    }
+
+    public void setMusicTitle(String musicTitle) {
+        this.musicTitle = musicTitle;
+    }
+
+    public String getMusicName() {
+        return musicName;
+    }
+
+    public void setMusicName(String musicName) {
+        this.musicName = musicName;
+    }
+
+    public String getMusicPath() {
+        return musicPath;
+    }
+
+    public void setMusicPath(String musicPath) {
+        this.musicPath = musicPath;
+    }
+
+    public String getMusicArtist() {
+        return musicArtist;
+    }
+
+    public void setMusicArtist(String musicArtist) {
+        this.musicArtist = musicArtist;
+    }
+
+    public int getMusicDuration() {
+        return musicDuration;
+    }
+
+    public void setMusicDuration(int musicDuration) {
+        this.musicDuration = musicDuration;
+    }
+}
